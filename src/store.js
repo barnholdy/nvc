a0fa57f434cb58c40d0874b128a7afe85cc4f889
Hannes Leitner <hannes.leitner@siili.com>
2018-09-06 18:49:22 +0200

    first draft of add felt feelings

--- src/store.js
+++ src/store.js
@@ -3,14 +3,69 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const FELT_FEELINGS_STORAGE_KEY = 'feltFeelings';
+
+function storageAvailable(type) {
+  try {
+    const storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (
+      // everything except Firefox
+      e.code === 22 ||
+      // Firefox
+      e.code === 1014 ||
+      // test name field too, because code might not be present
+      // everything except Firefox
+      e.name === 'QuotaExceededError' ||
+      // Firefox
+      e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage.length !== 0; // eslint-disable-line no-undef
+  }
+}
+
 export default new Vuex.Store({
   state: {
-
+    feltFeelings: [],
+  },
+  getters: {
+    feltFeelings: state => state.feltFeelings,
   },
   mutations: {
-
+    setFeltFeelings(state, feltFeelings) {
+      state.feltFeelings = feltFeelings; // eslint-disable-line no-param-reassign
+    },
+    addFeltFeelings(state, payload) {
+      const feltFeelings = {
+        fullfilled: payload.fullfilled,
+        unfullfilled: payload.unfullfilled,
+        time: +new Date(),
+      };
+      state.feltFeelings.push(feltFeelings);
+    },
   },
   actions: {
-
+    loadFeelings({ commit }) {
+      if (storageAvailable('localStorage')) {
+        const feltFeelingsJson = localStorage.getItem(FELT_FEELINGS_STORAGE_KEY);
+        if (feltFeelingsJson) {
+          commit('setFeltFeelings', JSON.parse(feltFeelingsJson));
+        }
+      } else {
+        throw new Error('Feelings were not loaded.');
+      }
+    },
+    saveFeelings({ commit, getters }, payload) {
+      commit('addFeltFeelings', payload);
+      if (storageAvailable('localStorage')) {
+        localStorage.setItem(FELT_FEELINGS_STORAGE_KEY, JSON.stringify(getters.feltFeelings));
+      } else {
+        throw new Error('Feelings were not saved persistently');
+      }
+    },
   },
 });
---
+++ src/views/FeelingAdd.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="feelings-add">
+    <h1>Wie geht es dir gerade?</h1>
+    <div class="feelings">
+      <div class="feelings--fullfilled">
+        <option-list
+          class="feelings__option"
+          heading="Erfüllt"
+          :options="fullfilledFeelings"
+          @optionSelected="addFullfilledFeeling">
+        </option-list>
+        <selection-list
+          :selectionList="selectedFeelingsFullfilled"
+          @selectionRemoved="removeFullfilledFeeling">
+        </selection-list>
+      </div>
+      <div class="feelings--unfullfilled">
+        <option-list
+          class="feelings__option"
+          heading="Unerfüllt"
+          :options="unfullfilledFeelings"
+          @optionSelected="addUnfullfilledFeeling">
+        </option-list>
+        <selection-list
+          :selectionList="selectedFeelingsUnfullfilled"
+          @selectionRemoved="removeUnfullfilledFeeling">
+        </selection-list>
+      </div>
+    </div>
+    <button @click="saveFeelings" :disabled="!isFeelingSelected">speichern</button>
+
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+    <div>
+      <code>{{ feltFeelings }}</code>
+    </div>
+  </div>
+</template>
+
+<script>
+import OptionList from '@/components/OptionList.vue';
+import SelectionList from '@/components/SelectionList.vue';
+import availableFeelings from '../assets/feelings_de.json';
+
+export default {
+  name: 'feelings-add',
+  components: {
+    OptionList,
+    SelectionList,
+  },
+  data() {
+    return {
+      selectedFeelingsFullfilled: [],
+      selectedFeelingsUnfullfilled: [],
+    };
+  },
+  computed: {
+    fullfilledFeelings() {
+      return availableFeelings.fullfilled;
+    },
+    unfullfilledFeelings() {
+      return availableFeelings.unfullfilled;
+    },
+    isFeelingSelected() {
+      return this.selectedFeelingsFullfilled.length > 0
+        || this.selectedFeelingsUnfullfilled.length > 0;
+    },
+    feltFeelings() {
+      return this.$store.getters.feltFeelings;
+    },
+  },
+  created() {
+    this.$store.dispatch('loadFeelings');
+  },
+  methods: {
+    addFullfilledFeeling(feeling) {
+      this.addFeeling(this.selectedFeelingsFullfilled, feeling);
+    },
+    removeFullfilledFeeling(feeling) {
+      this.removeFeeling(this.selectedFeelingsFullfilled, feeling);
+    },
+    addUnfullfilledFeeling(feeling) {
+      this.addFeeling(this.selectedFeelingsUnfullfilled, feeling);
+    },
+    removeUnfullfilledFeeling(feeling) {
+      this.removeFeeling(this.selectedFeelingsUnfullfilled, feeling);
+    },
+    addFeeling(feelings, feeling) {
+      const index = feelings.indexOf(feeling);
+      if (index === -1) {
+        feelings.push(feeling);
+      }
+    },
+    removeFeeling(feelings, feeling) {
+      const index = feelings.indexOf(feeling);
+      if (index !== -1) {
+        feelings.splice(index, 1);
+      }
+    },
+    saveFeelings() {
+      if (this.isFeelingSelected) {
+        this.$store.dispatch('saveFeelings', {
+          fullfilled: this.selectedFeelingsFullfilled,
+          unfullfilled: this.selectedFeelingsUnfullfilled,
+        });
+        this.resetFeelings();
+      }
+    },
+    resetFeelings() {
+      this.selectedFeelingsFullfilled = [];
+      this.selectedFeelingsUnfullfilled = [];
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.feelings {
+  display: flex;
+  flex-flow: row;
+  &--fullfilled,
+  &--unfullfilled {
+    flex-grow: 1;
+  }
+}
+</style>
